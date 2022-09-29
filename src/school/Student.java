9f7dd278a6951b26a3905c52b601634489b9c127
sm <yjspro1@gmail.com>
2022-09-29 15:40:26 +0900

    ADD : Student.java

---
+++ src/school/Student.java
@@ -0,0 +1,18 @@
+package school;
+
+import java.util.ArrayList;
+
+public class Student {
+    private int studentId;
+    private String studentName;
+    private Subject majorSubject;
+
+    private ArrayList<Score> scoreList = new ArrayList<Score>();
+
+    public Student(int studentId, String studentName, Subject majorSubject){
+        this.studentId = studentId;
+        this.studentName = studentName;
+        this.majorSubject = majorSubject;
+    }
+
+}
